15df7a7fd190ada384ecc8b8d30de9a9cc0677cc
Julio Briones <juliobriones@pop-os.localdomain>
2020-03-10 21:44:59 -0600

    Initial and final commit.

---
+++ generator/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+def home(request):
+  return render(request, "generator/home.html")
+
+def about(request):
+  return render(request, "generator/about.html")
+
+def password(request):
+  the_password = ""
+  length = int(request.GET.get("length",12))
+  characters = list("abcdefghijklmnopqrstuvwxyz")
+
+  if request.GET.get('uppercase'):
+    characters.extend(list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+  if request.GET.get('numbers'):
+    characters.extend(list("1234567890"))
+  if request.GET.get('special'):
+    characters.extend(list("!#$%&/[]*~\¬°"))
+
+  for x in range(length):
+    the_password += random.choice(characters)
+
+  return render(request, "generator/password.html", {"password": the_password})
+
